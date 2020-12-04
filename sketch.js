5548e0da3bca44e33b0c4412bb4ccc8770c01723
THECoder12345324 <sanjeevsharma.ait@gmail.com>
2020-12-04 16:54:48 -0500

    Making it interact with database to keep it in sync

--- sketch.js
+++ sketch.js
@@ -1,29 +1,61 @@
 var ball;
 
+var database, position;
+
 function setup(){
     createCanvas(500,500);
+
+    //Creates a connection with the database
+    database = firebase.database();
+
+    //Referring to the location of database where value is stored
+    var nodeposition = database.ref("Ball/position");
+
+    //Creates a listener which listens to changes in database
+    nodeposition.on("value", readOperation, showError);
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writeOperation(-1,0);
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+    if(keyDown(RIGHT_ARROW)){
+        writeOperation(1,0);
     }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+    if(keyDown(UP_ARROW)){
+        writeOperation(0,-1);
     }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+    if(keyDown(DOWN_ARROW)){
+        writeOperation(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function readOperation(data){
+
+    //Reads the value in the database
+    position = data.val();
+
+    //Sets the balls initial position
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function writeOperation(x, y) {
+
+    //Sets the value in the database
+    database.ref("Ball/position").set(
+        {x: ball.x + x, 
+         y: ball.y + y});
+}
+
+//In case there is an error
+function showError() {
+    console.log("There was an error");
 }
